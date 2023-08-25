390f51851314d9f2d977f99742906fa4e41b0b70
Neha <nehagholap0307@gmail.com>
2023-08-25 22:00:42 +0530

    linkedlist_singly

---
+++ LinkedList/LinkedList.java
@@ -0,0 +1,20 @@
+public class LinkedList {
+
+  public static void main(String[] args) {
+    LLDoubly list = new LLDoubly();
+    list.insertAtFirst(1);
+    list.insertAtFirst(0);
+    list.display();
+    // LLSingly list = new LLSingly();
+
+    // list.insertAtLast(0);
+    // list.insertAtLast(1);
+    // list.insertAtLast(2);
+    // list.insertAtLast(3);
+    // list.deleteAtIndex(0);
+    // list.insertAtFirst(0);
+    // System.out.println(list.findAtIndex(3));
+    // System.out.println(list.find(0));
+    // list.display();
+  }
+}
