89a860b6eac69d4bdcdaeeeb87bf7e73296579d9
Neha <nehagholap0307@gmail.com>
2023-08-26 20:28:51 +0530

    merge two sorted lists, leetcode 21

---
+++ LinkedList/mergeTwoSortedLists.java
@@ -0,0 +1,39 @@
+public class mergeTwoSortedLists {
+
+  public static void main(String[] args) {}
+
+  public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+      this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+      this.val = val;
+      this.next = next;
+    }
+  }
+
+  public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+    ListNode dummyHead = new ListNode();
+    ListNode tail = dummyHead;
+
+    while (list1 != null && list2 != null) {
+      if (list1.val <= list2.val) {
+        tail.next = list1;
+        list1 = list1.next;
+      } else {
+        tail.next = list2;
+        list2 = list2.next;
+      }
+      tail = tail.next;
+    }
+    tail.next = list1 == null ? list2 : list1;
+    return dummyHead.next;
+  }
+}
